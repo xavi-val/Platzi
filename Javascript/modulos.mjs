a560f5e719976157b57549b14a23bef010bb1e99
Javier Valdez <javier.valdez.15v@gmail.com>
2022-05-20 18:23:36 -0600

    Terminando curso intermedio de JS

--- Javascript/modulos.mjs
+++ Javascript/modulos.mjs
@@ -1,16 +1,31 @@
 export class persona {
-    constructor({nombre,amigo,apellido,twitter="No tiene",facebook }) {
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.amigo = amigo;
-    }
-  
-    saludar() {
-      console.log(`Hola ${this.amigo}`);
-    }
+  constructor({ nombre, amigo, apellido, twitter = "No tiene", facebook }) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.amigo = amigo;
   }
-  
-  let luis = new persona({ apellido: "Valdez", 
-  nombre: "Luis", 
-  amigo: "Fatima" });
-  luis.saludar();
+
+  saludar() {
+    console.log(`Hola ${this.amigo}`);
+  }
+}
+
+let luis = new persona({ apellido: "Valdez", nombre: "Luis", amigo: "Fatima" });
+luis.saludar();
+
+console.log(Object.getOwnPropertyDescriptors(luis));
+
+Object.defineProperty(luis, "altura", {
+  value: "1.75",
+  enumerable: true,
+  writable: true,
+  configurable: false,
+});
+
+if (luis instanceof persona) {
+  console.log("Es una persona");
+} else {
+  console.log("Es de tipo " + typeof luis);
+}
+
+console.log(Object.entries(luis));
